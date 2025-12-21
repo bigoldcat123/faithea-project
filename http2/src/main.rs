4eec6fe1fc29f436312cca12d4c26eb176dbbe7b
bigoldcat123 <a1456870251@163.com>
2025-12-21 20:03:07 +0800

    Add bytes

--- http2/src/main.rs
+++ http2/src/main.rs
@@ -1,12 +1,10 @@
-use std::str::Bytes;
-
 use h2::server;
-use http::{Response, StatusCode};
+use http::{Response, StatusCode, header::CONTENT_LENGTH};
 use tokio::net::TcpListener;
 
-#[tokio::main(flavor="current_thread")]
+#[tokio::main(flavor = "current_thread")]
 pub async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:5928").await.unwrap();
+    let listener = TcpListener::bind("127.0.0.1:8899").await.unwrap();
 
     // Accept all incoming TCP connections.
     loop {
@@ -17,24 +15,39 @@ pub async fn main() {
                 let mut h2 = server::handshake(socket).await.unwrap();
                 // Accept all inbound HTTP/2 streams sent over the
                 // connection.
-                while let Some(request) = h2.accept().await {
-                    let (mut request, mut respond) = request.unwrap();
+                while let Some(Ok(request)) = h2.accept().await {
+                    let (mut request, mut respond) = request;
                     while let Some(Ok(e)) = request.body_mut().data().await {
                         println!("Received request: {:?}", e);
+                        println!("{}",request.body().is_end_stream());
+
+                        if request.body().is_end_stream(){
+                            break;
+                        }
+
                     }
+                    println!("哈哈哈");
+
+                    let (tx, mut rx) = tokio::sync::mpsc::channel::<Response<String>>(64);
+                    tokio::spawn(async move {
+                        while let Some(r) = rx.recv().await {
+                            let (p, b) = r.into_parts();
+                            // Send the response back to the client
+                            let mut x = respond
+                                .send_response(Response::from_parts(p, ()), false)
+                                .unwrap();
+                            let a = bytes::Bytes::from_owner(b);
+                            x.send_data(a, true).unwrap();
+                        }
+                    });
                     // Build a response with no body
                     let response = Response::builder()
                         .header("content-length", "4")
                         .status(StatusCode::OK)
-                        .body(())
+                        .body("abcd".to_string())
                         .unwrap();
-
-                    // Send the response back to the client
-                    let mut x = respond.send_response(response ,false)
-                        .unwrap();
-                    x.send_data("helo".into(), true).unwrap();
+                    let _ = tx.send(response).await;
                 }
-
             });
         }
     }
